533f3e34c0b22612dc90807852295b68068b6877
fakirmohanpatra <fakirpatra789@gmail.com>
2025-02-22 21:54:12 +0530

    Main header components and some of navigation components were updated

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 
 import Users from './user/pages/User';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () =>  {
   return <Router>
+    <MainNavigation />
+    <main>
     <Switch>
       <Route path="/" exact>
         <Users />
@@ -17,6 +20,7 @@ const App = () =>  {
 
       <Redirect to="/" />
     </Switch>
+    </main>
   </Router>
 }
 
